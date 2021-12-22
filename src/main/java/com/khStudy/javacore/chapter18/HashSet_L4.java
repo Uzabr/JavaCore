8e7fbc00809419107c91f25e60dd16d7bf7bb562
Uzabr <khaitboboev94@mail.ru>
2021-12-22 23:42:55 +0300

    Core

---
+++ src/main/java/com/khStudy/javacore/chapter18/HashSet_L4.java
@@ -0,0 +1,17 @@
+package main.java.com.khStudy.javacore.chapter18;
+
+import java.util.HashSet;
+
+public class HashSet_L4 {
+
+  public static void main(String[] args) {
+
+      HashSet<String> hashSet = new HashSet<>();
+      hashSet.add("Bank");
+      hashSet.add("Cafe");
+      hashSet.add("Market");
+      hashSet.add("Service");
+      hashSet.add("Alfa");
+    System.out.println(hashSet);
+  }
+}
