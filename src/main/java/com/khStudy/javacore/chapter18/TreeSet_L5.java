a5e15ae2d70f0ff153c5c90beeed433dc1ed7e9a
Uzabr <khaitboboev94@mail.ru>
2021-12-23 11:06:33 +0300

    TreeSet

---
+++ src/main/java/com/khStudy/javacore/chapter18/TreeSet_L5.java
@@ -0,0 +1,16 @@
+package main.java.com.khStudy.javacore.chapter18;
+
+import java.util.TreeSet;
+
+public class TreeSet_L5 {
+
+  public static void main(String[] args) {
+    TreeSet<String> treeSet  = new TreeSet<>();
+    treeSet.add("C");
+    treeSet.add("V");
+    treeSet.add("N");
+    treeSet.add("B");
+    treeSet.add("A");
+    System.out.println(treeSet);
+  }
+}
