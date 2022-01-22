e7e35cdc800e1676b78942512f0427229603a7f1
Uzabr <khaitboboev94@mail.ru>
2022-01-22 11:06:18 +0300

    improve the solution to O(n logn) time complexity

--- src/main/java/com/khStudy/javacore/tasks/FindingDuplicates.java
+++ src/main/java/com/khStudy/javacore/tasks/FindingDuplicates.java
@@ -1,15 +1,15 @@
 package main.java.com.khStudy.javacore.tasks;
 
-public class FindingDuplicates {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-    
+public class FindingDuplicates {
     public static boolean findDuplicate(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-      for (int j = i + 1; j < array.length; j++) {
-          if (array[ i ] == array[ j ] )
-              return true;
+        Arrays.sort(array);
+    for (int i = 0; i < array.length - 1; i++) {
+      if (array[i] == array[i + 1]) return true;
       }
-        }
         return false;
     }
 }
