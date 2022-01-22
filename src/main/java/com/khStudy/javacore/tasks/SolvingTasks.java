b27466fd0cd83dac82d7a7c9f477fab366451554
Uzabr <khaitboboev94@mail.ru>
2022-01-22 11:23:51 +0300

    Time coplexity is O(n)

--- src/main/java/com/khStudy/javacore/tasks/SolvingTasks.java
+++ src/main/java/com/khStudy/javacore/tasks/SolvingTasks.java
@@ -1,15 +1,22 @@
 package main.java.com.khStudy.javacore.tasks;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SolvingTasks {
     public static int[] sumOfElementsWhichEqualsToGivenNumber(int[] array, int number) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
         for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == number)
-                    return new int[] {i, j};
+            if (map.containsKey(number - array[ i ])) {
+                result[0] = i;
+                result[1] = map.get(number - array[ i ]);
+            }
+            else {
+                map.put(array[i], i);
             }
         }
-        return new int[]{};
+        return result;
     }
 }
